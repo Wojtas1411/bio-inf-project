c1c7b17c374f4dcd440e548206b394b3ad3b1f3c
wojtas1411 <wojtas.t.1997@gmail.com>
2019-03-22 23:38:01 +0100

    threading template for instances processing

---
+++ Define.h
@@ -0,0 +1,10 @@
+//
+// Created by wojtek on 22.03.19.
+//
+
+#ifndef BIOINF_DEFINE_H
+#define BIOINF_DEFINE_H
+
+#define THREAD_NUM 8
+
+#endif //BIOINF_DEFINE_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <thread>
+#include "Define.h"
+
+//g++ -std=c++17 -Wall main.cpp -o ./cmake-build-debug/test -lstdc++fs --compile with gcc :)))
+
+void solve(std::string filename){
+    std::cout << filename << std::endl;
+    //TODO
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string input_path = "../instances";
+
+    // vector of filenames
+    auto *filenames = new std::vector<std::string>();
+
+    for(const auto & entry : std::filesystem::directory_iterator(input_path)){
+        filenames->push_back(entry.path());
+    }
+
+    //number of instances
+    size_t number_of_instances = filenames->size();
+
+    //tab of threads - one per instance
+    auto *threads = new std::thread[number_of_instances];
+
+    //iterator for thread dispatch
+    size_t iter = 0;
+
+    //thread dispatch - max THREAD_NUM threads at once
+    for(const auto & a : *filenames){
+
+        if(iter >= THREAD_NUM){
+            threads[iter-THREAD_NUM].join();
+        }
+
+        if(iter < number_of_instances){
+            threads[iter] = std::thread(solve, a);
+        }
+
+        iter++;
+    }
+
+    //close remaining threads
+    for(size_t i=iter-THREAD_NUM; i<iter; i++){
+        threads[i].join();
+    }
+
     return 0;
 }
